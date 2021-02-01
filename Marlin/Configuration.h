d54505f1015fe499d1bf8c0cbff2509447ce6f3d
exploitz86 <61384920+exploitz86@users.noreply.github.com>
2021-02-01 12:18:27 +0000

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1167,7 +1167,7 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -3
+#define X_MIN_POS 0
 #define Y_MIN_POS -2
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
